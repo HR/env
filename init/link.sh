5e0590f67cd7e8cf93cc0ac9171200052ccbc619
Habib <Habib@hr.local>
2020-01-31 21:28:57 +0000

    make link work

--- init/link.sh
+++ init/link.sh
@@ -4,9 +4,11 @@
 # Symlinks all dotfiles in repo in
 # $HOME path
 # #################################
-for file in $( ls -A ../ | grep -vE 'scripts$|\.git$|\.gitignore|.*.md' ); do
-	ln -sv "$PWD/$file" "$HOME"
-  echo "Symlinked $PWD/$file -> $HOME/$file"
+for file in $( ls -A ../ | grep -E '^\..+' ); do
+  SRC_PATH=$(dirname $PWD)/$file
+  LINK_PATH=$HOME/$file
+	ln -sv "$SRC_PATH" "$LINK_PATH"
+  echo "Symlinked $SRC_PATH -> $LINK_PATH"
 done
 
 echo "done."
